392c69f8bfc9640e2c2eca5ccf239ffbed5b64b9
jl <jlin@aaxischina.com>
2015-05-14 18:26:52 +0800

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JinBeiShaMei::Application.config.secret_key_base = 'bd6f6e38fcea4d438eaa196d0a553ae7c3c797b0cfe43fd0398d619055207ead9f5abac815f0d698b55c1ab209f4bfdd64004d03929f9557f5713d4977c96c89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JinBeiShaMei::Application.config.session_store :cookie_store, key: '_jin_bei_sha_mei_session'
